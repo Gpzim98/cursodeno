9a4ec48716c11914b1521efdd38683db3b77a908
Gregory Pacheco <gpzim98@gmail.com>
2020-05-23 13:12:13 +0100

    Adding missing pieces

--- website/urls.ts
+++ website/urls.ts
@@ -1,10 +1,19 @@
 import { HomeController, AboutController } from './apps/homepage/controller.ts';
+import { Router } from "https://deno.land/x/oak/mod.ts";
 
-export var urls = [
-    {reg: '^$', controller: new HomeController()},
-    {reg: '^/$', controller: new HomeController()},
-    {reg: '^/home$', controller: new HomeController()},
-    {reg: '^/home/$', controller: new HomeController()},
-    {reg: '^/about/$', controller: new AboutController()},
-    {reg: '^/administracao/$', controller: new HomeController()},
-]
+export var router = new Router();
+
+router
+    .get('/users', (context) => 
+        new HomeController().get(context))
+    .get('/about', (context) =>
+        new AboutController().returnResponse(context))
+    .post('/users', async (context) =>
+         new HomeController().post(context))
+    .put('/users/:id', (context) =>        
+        new HomeController().put(context))
+    .delete('/users/:id', (context) => 
+        new HomeController().delete(context))
+    .all('/users/:id', (context) => 
+        new HomeController().all(context)
+    );
